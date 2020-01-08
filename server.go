f950d4239e0dd51d2a334eb45a5b4debff114ce1
ChoiJunyong <windsdeath@gmail.com>
2020-01-09 00:00:18 +0900

    쿼리

---
+++ server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	
+	"github.com/labstack/echo"
+)
+
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/show", show)
+	e.Logger.Fatal(e.Start(":8000"))
+}
